efa14fc64d0a499608c5016aac5e19b1bdddf77f
bvlamas2010s-hub <bvlamas2010s@gmail.com>
2026-09-19 18:07:18 +0200

    Apply clean v0.7 defaults on upgrade

--- app/src/main/java/com/example/trajectoryoverlay/MainActivity.kt
+++ app/src/main/java/com/example/trajectoryoverlay/MainActivity.kt
@@ -40,6 +40,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         projectionManager = getSystemService(MediaProjectionManager::class.java)
+        Prefs.applyV07Defaults(this)
         openCvReady = OpenCVLoader.initLocal()
         requestNotificationPermission()
         setContentView(buildUi())
